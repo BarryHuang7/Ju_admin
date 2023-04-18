eb73346074b8f09bf8b6ee9d60529d26d9b0292d
Barry Huang <945912678@qq.com>
2023-04-18 14:42:02 +0800

    返回【客人】上传了多少文件的实体

---
+++ demo/Commons/src/main/java/com/example/commons/entity/GuestFileNumberVO.java
@@ -0,0 +1,10 @@
+package com.example.commons.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class GuestFileNumberVO implements Serializable {
+    private int number;
+}
