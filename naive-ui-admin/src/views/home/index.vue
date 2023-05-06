35b8f2c9e34ecb2ee433d47b663a01017af59d6d
Barry Huang <945912678@qq.com>
2023-05-06 10:08:24 +0800

    新增访客量显示

--- naive-ui-admin/src/views/home/index.vue
+++ naive-ui-admin/src/views/home/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div @click="test()">首页啥都没有呢！</div>
+  <div>
+    <div @click="test()" text-center cursor-pointer title="你点我试试？">首页啥都没有呢！</div>
+
+    <n-statistic tabular-nums mt-10>
+      <span>今日访客量：</span>
+      <span c-green>
+        <n-number-animation :from="0" :to="number" />
+      </span>
+      <template #suffix>人</template>
+    </n-statistic>
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import { ref } from 'vue';
+  import { toHttp } from '@/api/table/list';
+
+  const number = ref(0);
   let i = 0;
+
   const test = () => {
     i++;
     let msg = '你非要点是吧？';
@@ -12,4 +27,15 @@
     }
     window['$message'].warning(msg);
   };
+
+  const getVisitorNumber = async () => {
+    const url = '/h/getVisitorNumber';
+    const type = 'GET';
+
+    await toHttp(url, type).then((res) => {
+      number.value = res.data.number || 0;
+    });
+  };
+
+  getVisitorNumber();
 </script>
