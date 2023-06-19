7473142a48779b77efcf147808b99286b568e451
Barry Huang <945912678@qq.com>
2023-06-19 17:03:31 +0800

    新增发送弹幕功能

--- naive-ui-admin/src/views/swoole/websocket/index.vue
+++ naive-ui-admin/src/views/swoole/websocket/index.vue
@@ -4,14 +4,14 @@
       <vue3-barrage
         ref="barrage"
         :lanesCount="6"
-        boxWidth="500"
-        boxHeight="300"
-        :isShow="data.barrageIsShow"
-        :list="data.barrageList"
-        :loop="data.barrageLoop"
-        :speed="data.speed"
+        :boxWidth="500"
+        :boxHeight="300"
+        :isShow="barrageData.barrageIsShow"
+        :list="barrageData.barrageList"
+        :loop="barrageData.barrageLoop"
+        :speed="barrageData.speed"
         attachId="barrage"
-        :fontSize="data.fontSize"
+        :fontSize="barrageData.fontSize"
       >
         <!-- 自定义弹幕样式 -->
         <template #barrage="list">
@@ -21,7 +21,24 @@
     </div>
 
     <div flex justify-center items-center mt-10>
-      <n-input v-model:value="message" placeholder="请输入消息" style="width: 200px" />
+      <n-select
+        v-model:value="barrageData.position"
+        :options="barrageOptions"
+        style="width: 100px"
+      />
+      <n-space ml-10>
+        <n-switch v-model:value="barrageData.barrageIsShow">
+          <template #checked>弹幕关</template>
+          <template #unchecked>弹幕开</template>
+        </n-switch>
+      </n-space>
+      <n-input
+        v-model:value="message"
+        placeholder="请输入消息"
+        @keyup.enter="keyUp()"
+        style="width: 200px"
+        ml-10
+      />
       <n-button type="primary" ml-10 @click="sendMessage()">发送弹幕</n-button>
     </div>
   </div>
@@ -29,9 +46,19 @@
 
 <script lang="ts" setup>
   import { ref, reactive } from 'vue';
+  import { useUserStore } from '@/store/modules/user';
+
+  const userStore = useUserStore();
+  const userInfo: object = userStore.getUserInfo || {};
 
   // 弹幕位置：滚动、顶部固定、底部固定
   type PositionStatus = 'normal' | 'top' | 'bottom';
+
+  const barrageOptions = [
+    { label: '默认', value: 'normal' },
+    { label: '顶部', value: 'top' },
+    { label: '底部', value: 'bottom' },
+  ];
 
   interface BarrageList {
     id: number;
@@ -41,7 +68,7 @@
 
   const message = ref('');
 
-  let data = reactive({
+  let barrageData = reactive({
     // 弹幕列表
     barrageList: [] as Array<BarrageList>,
     // 弹幕位置
@@ -52,18 +79,41 @@
     barrageIsShow: true,
     currentId: 0,
     // 是否循环显示
-    barrageLoop: true,
+    barrageLoop: false,
     // 弹幕速度
     speed: 5,
   });
 
-  const sendMessage = () => {
-    data.barrageList.push({
-      id: ++data.currentId,
-      msg: message.value,
-      position: 'normal',
-    });
+  const ws = ref<WebSocket>();
 
-    message.value = '';
+  ws.value = new WebSocket('ws://175.178.236.223:9502/websocket');
+
+  ws.value.onmessage = (msg) => {
+    const data = JSON.parse(msg.data);
+
+    barrageData.barrageList.push({
+      id: ++data.currentId,
+      msg: `${data.name}说：${data.msg}`,
+      position: barrageData.position as PositionStatus,
+    });
+  };
+
+  const sendMessage = () => {
+    if (message.value) {
+      const msg_json = {
+        name: userInfo['name'],
+        msg: message.value,
+      };
+
+      if (ws.value) {
+        ws.value.send(JSON.stringify(msg_json));
+      }
+
+      message.value = '';
+    }
+  };
+
+  const keyUp = () => {
+    sendMessage();
   };
 </script>
